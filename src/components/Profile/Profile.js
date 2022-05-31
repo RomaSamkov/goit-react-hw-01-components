e20cc6d7dcaeb28d10009d99c8dd846712b78632
RomaSamkov <Romansamkov.rs@gmail.com>
2022-05-31 18:38:18 +0300

    add styless to profile module

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,26 +1,27 @@
+import s from './Profile.module.css';
 export default function Profile({ username, tag, location, avatar, stats }) {
   const { followers, views, likes } = stats;
   return (
-    <div class="profile">
-      <div class="description">
-        <img src={avatar} alt="User avatar" class="avatar" />
-        <p class="name">{username}</p>
-        <p class="tag">@{tag}</p>
-        <p class="location">{location}</p>
+    <div className={s.profile}>
+      <div className={s.description}>
+        <img src={avatar} alt="User avatar" className={s.avatar} />
+        <p className={s.name}>{username}</p>
+        <p className={s.tag}>@{tag}</p>
+        <p className={s.location}>{location}</p>
       </div>
 
-      <ul class="stats">
+      <ul className={s.stats}>
         <li>
-          <span class="label">Followers</span>
-          <span class="quantity">{followers}</span>
+          <span className={s.label}>Followers</span>
+          <span className={s.quantity}>{followers}</span>
         </li>
         <li>
-          <span class="label">Views</span>
-          <span class="quantity">{views}</span>
+          <span className={s.label}>Views</span>
+          <span className={s.quantity}>{views}</span>
         </li>
         <li>
-          <span class="label">Likes</span>
-          <span class="quantity">{likes}</span>
+          <span className={s.label}>Likes</span>
+          <span className={s.quantity}>{likes}</span>
         </li>
       </ul>
     </div>
