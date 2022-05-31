fac9f24f49df8e5a3d40bf95841c937c43814876
RomaSamkov <Romansamkov.rs@gmail.com>
2022-05-31 18:52:10 +0300

    Update Profile.js

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,6 +1,9 @@
 import s from './Profile.module.css';
+import PropTypes from 'prop-types';
+
 export default function Profile({ username, tag, location, avatar, stats }) {
   const { followers, views, likes } = stats;
+
   return (
     <div className={s.profile}>
       <div className={s.description}>
@@ -27,3 +30,15 @@ export default function Profile({ username, tag, location, avatar, stats }) {
     </div>
   );
 }
+
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }),
+};
