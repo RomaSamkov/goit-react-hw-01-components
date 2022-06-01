4f9db05e46fda4b6bee6d905bf05f8b3b3622d79
RomaSamkov <Romansamkov.rs@gmail.com>
2022-06-01 17:44:14 +0300

    create TransactionHistory

--- src/components/FriendList/FriendListItem/FriendListItem.js
+++ src/components/FriendList/FriendListItem/FriendListItem.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 export default function FriendListItem({ avatar, name, isOnline }) {
   return (
     <li className={s.item}>
-      <span className={s.status}>{isOnline}</span>
+      <span className={isOnline ? s.active : s.status}></span>
       <img className={s.avatar} src={avatar} alt="User avatar" width="48" />
       <p className={s.name}>{name}</p>
     </li>
